f4c7ea70c370a30fa6933bb70b884ca7906a793b
HuangD <996497144@qq.com>
2016-11-16 16:15:54 +0800

    分页架构

---
+++ QMM-m/js/new.js
@@ -0,0 +1,38 @@
+ $(function() {
+        // 返回顶部
+        $(window).bind("scroll.gotop", function() {
+            if ($(window).scrollTop() > 30) {
+                $(".back").fadeIn('400').css("display", "block");
+            } else {
+                $(".back").fadeOut('400')
+            }
+        })
+        $(".back").click(function() {
+            $(document).scrollTop(0);
+        });
+        // 登录条
+        $(".login").click(function(e) {
+            if (!$(".sidbar").is(":visible")) {
+                $(".sidbar").show();
+            } else {
+                $(".sidbar").hide();
+            }
+
+            e.stopPropagation();
+        });
+
+        $(document).click(function() {
+            $(".sidbar").hide();
+        });
+
+        $(".sidbar").click(function(e) {
+            e.stopPropagation();
+        });
+    })
+
+
+    var slide = document.querySelector('.swipe');
+    Swipe(slide, {
+        auto: 3000,
+        speed: 400
+    });
